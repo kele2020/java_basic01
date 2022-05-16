0a0e93f8471af3cfef3c9c6f2add6dcfd9334125
phy <phy70000@163.com>
2022-05-17 00:43:17 +0800

    添加文件

---
+++ untitled3-empty/untitled-module1/src/package1/com/HelloWorld.java
@@ -0,0 +1,55 @@
+package package1.com;
+/*
+java程序中最基本的组成单位是类
+*/
+
+import java.util.Scanner;
+
+
+/*
+关键字
+
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+     /*   System.out.println("Hello 项目 模块 src 包 类");
+        System.out.println('A');
+        System.out.println(88);
+        boolean b= true;
+        System.out.println(b);
+        b = false;
+        System.out.println(b);
+        long l = 10000000000l; *//*需要+l*//*
+        System.out.println(l);
+        float f = 11.11f;    *//*需要+f*//*
+        System.out.println(f);
+        int t = 1;        *//*整数默认类型*//*
+        System.out.println(t);
+        double d =1.1;   *//*浮点数默认类型*//*
+        System.out.println(d);
+
+        int d1 = 5;
+        double i1 = 2;
+        double c = d1/i1;
+        System.out.println(c);
+
+        char c1 = '0';
+        int i2 = c1;
+        System.out.println(i2);*/
+  /*
+ 
+        short j = 10;
+        j = (short)(j + 5) ;
+
+        System.out.println( i);
+        System.out.println( j);*/
+
+        int a = 1;
+        int b = 2;
+        System.out.println("begin"
+        );
+
+
+    }
+}
---
+++ xunhuan/xunhuan-module/src/package_xunhuan/HelloWorld.java
@@ -0,0 +1,13 @@
+package package_xunhuan;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int s=0;
+        do {
+            System.out.println("s:" + s);
+            s++;
+        }
+        while (s<9);
+
+    }
+}
